c94b0d1ab04998901aa7fd4907348d0942aa7a04
SohamGhugare <soham.ghugare@gmail.com>
2023-04-22 20:03:55 +0530

    Added albums model and few dummy albums

--- gin-api/api.go
+++ gin-api/api.go
@@ -1,3 +1,25 @@
 package main
 
+// Album struct for storing albums
+type album struct {
+  ID string `json:"id"`
+  Title string `json:"title"`
+  Artist string `json:"artist"`
+  Price float64 `json:"price"`
+}
 
+// Creating dummy albums
+var albums = []album{
+  {
+    ID: "1",
+    Title: "Blue Train",
+    Artist:"John Coltrane",
+    Price: 56.99,
+  },
+  {
+    ID: "2",
+    Title: "Jeru",
+    Artist: "Gerry Mulligan",
+    Price: 17.99,
+  },
+}
